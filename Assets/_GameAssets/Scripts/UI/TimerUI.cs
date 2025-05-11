05f3e24ba42245408b430a9df246a2aab29b12a2
Nail-Ui <nailbatuhan1@gmail.com>
2025-05-11 13:07:59 +0300

    V0.26 CutSceneAdded

--- Assets/_GameAssets/Scripts/UI/TimerUI.cs
+++ Assets/_GameAssets/Scripts/UI/TimerUI.cs
@@ -22,9 +22,6 @@ public class TimerUI : MonoBehaviour
 
     private void Start()
     {
-        PlayRotationAnimation();
-        StartTimer();
-        
         GameManager.Instance.OnGameStateChanged += GameManager_OnGameStateChanged;
     }
 
@@ -32,6 +29,10 @@ public class TimerUI : MonoBehaviour
     {
         switch(gameState)
         {
+            case GameState.Play:
+                PlayRotationAnimation();
+                StartTimer();
+                 break;
             case GameState.Pause:
                 StopTimer();
                 break;
